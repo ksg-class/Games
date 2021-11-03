6594f1c7f5889c56dfca9abab5507df448f19411
ksg42069 <ketchupash378@gmail.com>
2021-11-03 12:40:57 +0530

    pause menu

---
+++ miniProject/Assets/Scripts/Sound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    public static AudioMixer mv, sv;
+    public void Volume (float vol)
+    {
+        Debug.Log(vol);
+        mv.SetFloat("MusicVol", vol);
+        sv.SetFloat("SFXVolume", vol);
+    }
+}
